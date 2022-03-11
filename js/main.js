092c4fce3f9a94ce5d2f9c7b63727c0b03375e12
sravanthimodepu <sravanthimodepu@gmail.com>
2022-03-11 16:21:19 +0530

    added controllers funtionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-// slider for mass of bob
+// slider for load of arrow
 
 document.addEventListener('DOMContentLoaded', function() {  
-    let mass = 600;
+    let mass= 600;
     // slider for mass of bob
     const slider_mass = document.getElementById("mass");
     const output_mass = document.getElementById("id_mass");
@@ -12,35 +12,35 @@ document.addEventListener('DOMContentLoaded', function() {
         updatePara();
     };
 
-    let myAnimation = anime({
-        targets: '#rod',
-        rotate: [displacement, -1 * displacement],
-        duration: function(){
-            return time;
-        },
-        easing: 'easeInOutSine',
-        direction: 'alternate',
-        loop: true,
-    });   
 
     function updatePara()
     {   
         mass = document.getElementById("mass").value;       
-        const ball_css = document.querySelector("#ball");
+        const arrow_css = document.querySelector("#arrow");
 
         
-        ball_css.style.height = mass / 300 + "em" ;
-        ball_css.style.width = mass / 300 + "em" ;
-        ball_css.style.left = mass / (-600) + "em" ;
+        arrow_css.style.height = mass / 300 + "em" ;
+        arrow_css.style.width = mass / 300 + "em" ;
+        arrow_css.style.left = mass / (-600) + "em" ;
 
     }
 })
 
+var id = null;
+
+function setDefaults(){
+    var arrow = document.getElementById("arrow");
+    arrow.style.marginLeft='315px';
+    arrow.style.top='0px';
+
+ }
+
+
 // control buttons : play, pause ,restart
-var slowAnimation = anime({
-  targets: '.ball',
-  translateY: 100,
-  borderRadius: 50,
+let slowAnimation = anime({
+  targets: '.arrow',
+  translateY: 2,
+  borderRadius: 20,
   duration: 4000,
   easing: 'linear',
   autoplay: false
@@ -51,34 +51,38 @@ document.querySelector('#pause').onclick = slowAnimation.pause;
 document.querySelector('#restart').onclick = slowAnimation.restart;
 
 
-//
+// point load
 function myFunction() {
     if (document.getElementById("radio-style_point_load").checked) {
-        var elem = document.getElementById("ball");
-		var rect = document.getElementById("beam");
-		var bbox = rect.getBBox();
-		var y = 135 - (bbox.height - 50)/2
+        var elem = document.getElementById("arrow");
+        var rect = document.getElementById("rectangle");
+        var bbox = rect.getBBox();
+        var y = 135 - (bbox.height - 50)/2
 console.log(elem.style.top,bbox.height,y)
+        // var play= document.getElementById("#play");
+        // var pause=document.getElementById("#pause");
+        // var restart=document.getElementById("#restart");
+
         var posy = 0;
         clearInterval(id);
         id = setInterval(frame, 10);
 
 function animate(){
-anime({
+anime({ 
       //target
-      targets: "#beam",
-    //Properties
-    //rotateY: 360,
-    //scale: 0.5,
-   // translateX: 300,
-   // skew: 30,
-   // Property Parameters
-   // duration: 1000,
+      targets: "#rectangle",
+      //Properties
+      //rotateY: 360,
+      //scale: 0.5,
+     // translateX: 300,
+     // skew: 30,
+      // Property Parameters
+     // duration: 1000,
       endDelay: 300,
       easing: "easeInOutSine",
-   // Animation Parameters
-   // direction: "alternate",
-      rotate: [0, 5],
+      // Animation Parameters
+     // direction: "alternate",
+ rotate: [0, 5],
       duration: 4000,
 
       loop: false,
@@ -93,7 +97,7 @@ anime({
     });
  anime({
       targets: "#triangle1",
-      opacity: 1,
+opacity: 1,
       duration: 4000,
       endDelay: 300,
       easing: "easeInOutSine",
@@ -110,14 +114,29 @@ anime({
 setTimeout(animate,800);
 
             } else {
-				posy++;
+                posy++;
                 elem.style.top = posy + 'px';
             }
         }
     }
 
-}
 
+// control buttons : play, pause ,restart
+
+// slowAnimation = anime({
+//   targets: '.arrow',
+//   translateY: 68,
+//   borderRadius: 20,
+//   duration: 4000,
+//   easing: 'linear',
+//   autoplay: false
+// });
+
+// document.querySelector('#play').onclick = slowAnimation.play;
+// document.querySelector('#pause').onclick = slowAnimation.pause;
+// document.querySelector('#restart').onclick = slowAnimation.restart;
+
+}
 
 function getOption() {
     selectElement = document.querySelector('#menu');
@@ -144,13 +163,13 @@ function adjustRectLength() {
 document.getElementById('triangle1').style.display = 'none'
 anime({
       targets: "#rectangle",
-	  rotate: [0, 0],
+     rotate: [0, 0],
       duration: 1,
       loop: false,
     });
 anime({
       targets: "#observations_point_load_sfd",
-	  width:0,
+     width:0,
       duration: 1,
       loop: false,
     });
@@ -161,20 +180,20 @@ anime({
     var width = (len * 550) / 100;
     var diff = width - orginalLen;
     var pos = 0;
-	var elem = document.getElementById("ball");
-	var ballMarginfLeft = parseInt((elem.style.marginLeft).split('px'));
+    var elem = document.getElementById("arrow");
+    var arrowMarginfLeft = parseInt((elem.style.marginLeft).split('px'));
     clearInterval(id);
-	var left = 315;
-	if(len === 100) left = 540;
-	else if(len === 0) left = 90;
-	else left = 90 + 4.5*len;
-	elem.style.marginLeft =  left + 'px';
-	elem.style.top = '0px';
+    var left = 415;
+    if(len === 100) left = 540;
+    else if(len === 0) left = 90;
+    else left = 90 + 4.5*len;
+    elem.style.marginLeft =  left + 'px';
+    elem.style.top = '0px';
     id = setInterval(frame, 10);
     function frame() {
         if (pos > Math.abs(diff)) {
             clearInterval(id);
-			
+            
         } else {
             pos++;
             if (diff < 0) {
@@ -184,7 +203,7 @@ anime({
             }
         }
     }
-	
+    
 }
 
 
@@ -192,28 +211,28 @@ function adjustRectBreadth() {
 document.getElementById('triangle1').style.display = 'none'
 anime({
       targets: "#rectangle",
-	  rotate: [0, 0],
+     rotate: [0, 0],
       duration: 1,
       loop: false,
     });
 anime({
       targets: "#observations_point_load_sfd",
-	  width:0,
+     width:0,
       duration: 1,
       loop: false,
     });
-	var rect = document.getElementById("rectangle");
+    var rect = document.getElementById("rectangle");
     var bbox = rect.getBBox();
     var orginalheight = bbox.height;
-	var orginaly = bbox.y;
+    var orginaly = bbox.y;
     var height = document.getElementById("sliderWithValueTooltipBreadth").value;
-	var y = Math.floor(height/2);
+    var y = Math.floor(height/2);
     var diff = height - orginalheight;
     var pos = 0, posy = 0;
-	var elem = document.getElementById("ball");
+    var elem = document.getElementById("arrow");
     clearInterval(id);
-	var top = 0;
-	elem.style.top = '0px';
+    var top = 0;
+    elem.style.top = '0px';
     clearInterval(id);
     id = setInterval(frame, 10);
     function frame() {
@@ -221,13 +240,13 @@ anime({
             clearInterval(id);
         } else {
             pos++;
-			posy = posy+0.5;
+            posy = posy+0.5;
             if (diff < 0) {
                 rect.setAttribute("height", orginalheight - pos);
-				rect.setAttribute("y", orginaly + posy);
+                rect.setAttribute("y", orginaly + posy);
             } else {
                 rect.setAttribute("height", orginalheight + pos);
-				rect.setAttribute("y", orginaly - posy);
+                rect.setAttribute("y", orginaly - posy);
             }
         }
     }
