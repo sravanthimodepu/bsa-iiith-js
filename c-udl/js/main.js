ca0f0162102d33ec22b29b3ccfc7e77fed4743dd
sravanthimodepu <sravanthimodepu@gmail.com>
2022-03-22 11:02:40 +0530

    added animations for all the boundary types

--- c-udl/js/main.js
+++ c-udl/js/main.js
@@ -26,11 +26,14 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 })
 
+const instrMsg = document.getElementById('procedure-message');
+
 var id = null;
 
 function setDefaults(){
     var arrow = document.getElementById("arrow");
-    arrow.style.marginLeft='315px';
+    arrow.style.marginLeft='155px';
+    arrow.style.width='300px';
     arrow.style.top='0px';
 
  }
@@ -50,10 +53,10 @@ document.querySelector('#play').onclick = slowAnimation.play;
 document.querySelector('#pause').onclick = slowAnimation.pause;
 document.querySelector('#restart').onclick = slowAnimation.restart;
 
-
-// point load
-function myPoint() {
-    if (document.getElementById("radio-style_point_load").checked) {
+    
+// uniformly distributed load
+function mySecondFunction() {
+    if (document.getElementById("radio-style_ud_load").checked) {
         var elem = document.getElementById("arrow");
         var rect = document.getElementById("rectangle");
         var bbox = rect.getBBox();
@@ -71,31 +74,54 @@ function animate(){
 anime({ 
       //target
       targets: "#rectangle",
-      endDelay: 300,
+      endDelay: 400,
       easing: "easeInOutSine",
- rotate: [0, 5],
+      rotate: [0, 10],
       duration: 4000,
+
       loop: false,
     });
 anime({
-      targets: "#observations_point_load_sfd",
-      width: '100%',
-      endDelay: 300,
-      easing: "easeInOutSine",
-      duration: 4000,
-      loop: false,
-    });
- anime({
-      targets: "#triangle1",
-opacity: 1,
-      duration: 4000,
-      endDelay: 300,
-      easing: "easeInOutSine",
-      loop: false,
- begin: function() {
-    document.querySelector('#triangle1').style.display = 'block';
-  },
-    });
+    targets: ".svg-sfd path",
+    duration: 1000,
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: "easeInOutSine",
+    direction: "alternate",
+    delay: anime.stagger(200),
+    d: [
+        {
+            value:
+                "M 100 300 L 450 300 L 100 400 L 100 300 L 100 300 "
+        },
+        {
+            value:
+                "M 100 300 L 450 300 L 100 400 L 100 300 L 100 300 "
+        }
+    ],
+    loop: false
+
+});
+anime({
+    targets: ".svg-bmd path",
+    duration: 1000,
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: "easeInOutSine",
+    direction: "alternate",
+    delay: anime.stagger(200),
+    d: [
+        {
+            value:
+                "M 100 300 Q 250 300 500 300 C 350 300 150 350 100 400 Q 100 400 100 300 "
+        },
+        {
+            value:
+                "M 100 300 Q 250 300 500 300 C 350 300 150 350 100 400 Q 100 400 100 300 "
+        }
+    ],
+    loop: false
+
+});
+ 
 }
 
         function frame() {
@@ -110,6 +136,21 @@ setTimeout(animate,800);
         }
     }
 
+
+// control buttons : play, pause ,restart
+
+// slowAnimation = anime({
+//   targets: '.arrow',
+//   translateY: 68,
+//   borderRadius: 20,
+//   duration: 4000,
+//   easing: 'linear',
+//   autoplay: false
+// });
+
+// document.querySelector('#play').onclick = slowAnimation.play;
+// document.querySelector('#pause').onclick = slowAnimation.pause;
+// document.querySelector('#restart').onclick = slowAnimation.restart;
 
 }
 
@@ -142,12 +183,31 @@ anime({
       duration: 1,
       loop: false,
     });
-anime({
-      targets: "#observations_point_load_sfd",
-      width:0,
-      duration: 1,
-      loop: false,
-    });
+// anime({
+//       targets: "#observations_point_load_sfd",
+//       width:0,
+//       duration: 1,
+//       loop: false,
+//     });
+// anime({
+//     targets: ".svg path",
+//     duration: 1000,
+//     strokeDashoffset: [anime.setDashoffset, 0],
+//     easing: "easeInOutSine",
+//     direction: "alternate",
+//     delay: anime.stagger(200),
+//     d: [
+//         {
+//             value:
+//                 "M 100 150 L 300 150 L 100 150 L 100 250 L 300 150"
+//         },
+//         {
+//             value:
+//                 "M 100 150 L 300 150 L 100 150 L 100 250 L 300 150"
+//         }
+//     ],
+//     loop: false
+// });
     var rect = document.getElementById("rectangle");
     var bbox = rect.getBBox();
     var orginalLen = bbox.width;
@@ -158,7 +218,7 @@ anime({
     var elem = document.getElementById("arrow");
     var arrowMarginfLeft = parseInt((elem.style.marginLeft).split('px'));
     clearInterval(id);
-    var left = 415;
+    var left = 155;
     if(len === 100) left = 540;
     else if(len === 0) left = 90;
     else left = 90 + 4.5*len;
@@ -190,12 +250,31 @@ anime({
       duration: 1,
       loop: false,
     });
-anime({
-      targets: "#observations_point_load_sfd",
-     width:0,
-      duration: 1,
-      loop: false,
-    });
+// anime({
+//       targets: "#observations_point_load_sfd",
+//      width:0,
+//       duration: 1,
+//       loop: false,
+//     });
+// anime({
+//     targets: ".svg path",
+//     duration: 1000,
+//     strokeDashoffset: [anime.setDashoffset, 0],
+//     easing: "easeInOutSine",
+//     direction: "alternate",
+//     delay: anime.stagger(200),
+//     d: [
+//         {
+//             value:
+//                 "M 100 150 L 300 150 L 100 150 L 100 250 L 300 150"
+//         },
+//         {
+//             value:
+//                 "M 100 150 L 300 150 L 100 150 L 100 250 L 300 150"
+//         }
+//     ],
+//     loop: true
+// });
     var rect = document.getElementById("rectangle");
     var bbox = rect.getBBox();
     var orginalheight = bbox.height;
@@ -227,64 +306,39 @@ anime({
     }
 }
 
-// uniform distributed load
-function mySecondFunction() {
-    if (document.getElementById("radio-style").checked) {
-        var elem = document.getElementById("arrow");
-        var rect = document.getElementById("rectangle");
-        var bbox = rect.getBBox();
-        var y = 135 - (bbox.height - 50)/2
-console.log(elem.style.top,bbox.height,y)
-        // var play= document.getElementById("#play");
-        // var pause=document.getElementById("#pause");
-        // var restart=document.getElementById("#restart");
 
-        var posy = 0;
-        clearInterval(id);
-        id = setInterval(frame, 10);
 
-function animate(){
-anime({ 
-      //target
-      targets: "#rectangle",
-      endDelay: 300,
-      easing: "easeInOutSine",
-      rotate: [0, 5],
-      duration: 4000,
-      loop: false,
-    });
+const border = "black", lineWidth = 1.5, fps = 150;
+const msgs = [
+        "Click on 'Container' in the apparatus menu to add a container to the workspace.",
+        "Click on 'Soil Sample' in the apparatus menu to add a soil sample to the container.",
+        "Click on 'Water' in the apparatus menu to add water to the soil filled container.",
+        "Click on the soil to mix the soil with the water.",
+        "Click on 'Casagrande Apparatus' in the apparatus menu to add a casagrande cup to the workspace.", 
+        "Click on the container to move the soil to the casagrande apparatus as a soil pat.",
+        "Click on the casagrande apparatus to cut a groove in the soil pat.",
+        "Click on the casagrande apparatus to rotate the cup. Finally, determine the water content of the soil sample. Use the following <a href='https://sd-iiith.vlabs.ac.in/exp/water-content/'>link</a> to learn more about water content determination.",
+        "Click the restart button to perform the experiment again.",
+    ];
+
+
+
 anime({
-      targets: "#observations_point_load_sfd",
-      width: '100%',
-      endDelay: 300,
-      easing: "easeInOutSine",
-      duration: 4000,
-      loop: false,
-    });
- anime({
-      targets: "#triangle1",
-opacity: 1,
-      duration: 4000,
-      endDelay: 300,
-      easing: "easeInOutSine",
-      loop: false,
- begin: function() {
-    document.querySelector('#triangle1').style.display = 'block';
-  },
-    });
-}
-
-        function frame() {
-            if (posy > y ) { 
-                clearInterval(id);
-setTimeout(animate,800);
-
-            } else {
-                posy++;
-                elem.style.top = posy + 'px';
-            }
+    targets: ".svg path",
+    duration: 1000,
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: "easeInOutSine",
+    direction: "alternate",
+    delay: anime.stagger(200),
+    d: [
+        {
+            value:
+                "M 100 150 L 300 150 L 100 150 L 100 250 L 300 150"
+        },
+        {
+            value:
+                "M 100 150 L 300 150 L 100 150 L 100 250 L 300 150"
         }
-    }
-
-
-}
+    ],
+    loop: false
+});
--- twoss/twoss-udl/js/main.js
+++ twoss/twoss-udl/js/main.js
@@ -26,11 +26,14 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 })
 
+const instrMsg = document.getElementById('procedure-message');
+
 var id = null;
 
 function setDefaults(){
     var arrow = document.getElementById("arrow");
-    arrow.style.marginLeft='315px';
+    arrow.style.marginLeft='155px';
+    arrow.style.width='300px';
     arrow.style.top='0px';
 
  }
@@ -50,10 +53,10 @@ document.querySelector('#play').onclick = slowAnimation.play;
 document.querySelector('#pause').onclick = slowAnimation.pause;
 document.querySelector('#restart').onclick = slowAnimation.restart;
 
-
-// point load
-function myPoint() {
-    if (document.getElementById("radio-style_point_load").checked) {
+    
+// uniformly distributed load
+function mySecondFunction() {
+    if (document.getElementById("radio-style_ud_load").checked) {
         var elem = document.getElementById("arrow");
         var rect = document.getElementById("rectangle");
         var bbox = rect.getBBox();
@@ -71,31 +74,53 @@ function animate(){
 anime({ 
       //target
       targets: "#rectangle",
-      endDelay: 300,
+      endDelay: 400,
       easing: "easeInOutSine",
- rotate: [0, 5],
+      rotate: [0, 10],
       duration: 4000,
+
       loop: false,
     });
 anime({
-      targets: "#observations_point_load_sfd",
-      width: '100%',
-      endDelay: 300,
-      easing: "easeInOutSine",
-      duration: 4000,
-      loop: false,
-    });
+    targets: ".svg-sfd path",
+    duration: 1000,
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: "easeInOutSine",
+    direction: "alternate",
+    delay: anime.stagger(200),
+    d: [
+        {
+            value:
+                "M 150 150 L 450 150 L 450 250 L 150 50 L 150 150"
+        },
+        {
+            value:
+                "M 150 150 L 450 150 L 450 250 L 150 50 L 150 150"
+        }
+    ],
+    loop: false
+
+});
  anime({
-      targets: "#triangle1",
-opacity: 1,
-      duration: 4000,
-      endDelay: 300,
-      easing: "easeInOutSine",
-      loop: false,
- begin: function() {
-    document.querySelector('#triangle1').style.display = 'block';
-  },
-    });
+    targets: ".svg-bmd path",
+    duration: 1000,
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: "easeInOutSine",
+    direction: "alternate",
+    delay: anime.stagger(200),
+    d: [
+        {
+            value:
+                "M 100 300 L 400 300 Q 250 150 100 300 "
+        },
+        {
+            value:
+                "M 100 300 L 400 300 Q 250 150 100 300 "
+        }
+    ],
+    loop: false
+
+});
 }
 
         function frame() {
@@ -110,6 +135,21 @@ setTimeout(animate,800);
         }
     }
 
+
+// control buttons : play, pause ,restart
+
+// slowAnimation = anime({
+//   targets: '.arrow',
+//   translateY: 68,
+//   borderRadius: 20,
+//   duration: 4000,
+//   easing: 'linear',
+//   autoplay: false
+// });
+
+// document.querySelector('#play').onclick = slowAnimation.play;
+// document.querySelector('#pause').onclick = slowAnimation.pause;
+// document.querySelector('#restart').onclick = slowAnimation.restart;
 
 }
 
@@ -142,12 +182,6 @@ anime({
       duration: 1,
       loop: false,
     });
-anime({
-      targets: "#observations_point_load_sfd",
-      width:0,
-      duration: 1,
-      loop: false,
-    });
     var rect = document.getElementById("rectangle");
     var bbox = rect.getBBox();
     var orginalLen = bbox.width;
@@ -158,7 +192,7 @@ anime({
     var elem = document.getElementById("arrow");
     var arrowMarginfLeft = parseInt((elem.style.marginLeft).split('px'));
     clearInterval(id);
-    var left = 415;
+    var left = 155;
     if(len === 100) left = 540;
     else if(len === 0) left = 90;
     else left = 90 + 4.5*len;
@@ -187,12 +221,6 @@ document.getElementById('triangle1').style.display = 'none'
 anime({
       targets: "#rectangle",
      rotate: [0, 0],
-      duration: 1,
-      loop: false,
-    });
-anime({
-      targets: "#observations_point_load_sfd",
-     width:0,
       duration: 1,
       loop: false,
     });
@@ -227,64 +255,10 @@ anime({
     }
 }
 
-// uniform distributed load
-function mySecondFunction() {
-    if (document.getElementById("radio-style").checked) {
-        var elem = document.getElementById("arrow");
-        var rect = document.getElementById("rectangle");
-        var bbox = rect.getBBox();
-        var y = 135 - (bbox.height - 50)/2
-console.log(elem.style.top,bbox.height,y)
-        // var play= document.getElementById("#play");
-        // var pause=document.getElementById("#pause");
-        // var restart=document.getElementById("#restart");
-
-        var posy = 0;
-        clearInterval(id);
-        id = setInterval(frame, 10);
-
-function animate(){
-anime({ 
-      //target
-      targets: "#rectangle",
-      endDelay: 300,
-      easing: "easeInOutSine",
-      rotate: [0, 5],
-      duration: 4000,
-      loop: false,
-    });
-anime({
-      targets: "#observations_point_load_sfd",
-      width: '100%',
-      endDelay: 300,
-      easing: "easeInOutSine",
-      duration: 4000,
-      loop: false,
-    });
- anime({
-      targets: "#triangle1",
-opacity: 1,
-      duration: 4000,
-      endDelay: 300,
-      easing: "easeInOutSine",
-      loop: false,
- begin: function() {
-    document.querySelector('#triangle1').style.display = 'block';
-  },
-    });
-}
-
-        function frame() {
-            if (posy > y ) { 
-                clearInterval(id);
-setTimeout(animate,800);
-
-            } else {
-                posy++;
-                elem.style.top = posy + 'px';
-            }
-        }
-    }
 
 
-}
+const border = "black", lineWidth = 1.5, fps = 150;
+const msgs = [
+        
+    ];
+
